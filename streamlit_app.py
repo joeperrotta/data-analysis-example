a48b016af1372264d14faee9d287a21b43c4e658
joeperrotta <56735310+joeperrotta@users.noreply.github.com>
2022-05-19 20:10:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ from streamlit_pandas_profiling import st_profile_report
 import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.mixture import GaussianMixture
+from pylab import concatenate, normal
 
 st.set_page_config(
     layout="centered", page_icon="JP️", page_title="Data Analysis App"
@@ -83,13 +86,44 @@ with st.expander("View in Depth Analysis", expanded=False):
 
 
 
-N=10000
-mu, sigma = 100, 5
-mu2, sigma2 = 10, 40
-X1 = np.random.normal(mu, sigma, N)
-X2 = np.random.normal(mu2, sigma2, N)
-X = np.concatenate([X1, X2])
+# N=10000
+# mu, sigma = 1, .005
+# mu2, sigma2 = .1, .04
+# X1 = np.random.normal(mu, sigma, N)
+# X2 = np.random.normal(mu2, sigma2, N)
+# X = np.concatenate([X1, X2])
+# st.line_chart(X)
+# st.altair_chart(X)
+
+
+# First normal distribution parameters
+mu1 = 1
+sigma1 = 0.1
+
+# Second normal distribution parameters
+mu2 = 2
+sigma2 = 0.2
+
+w1 = 2/3 # Proportion of samples from first distribution
+w2 = 1/3 # Proportion of samples from second distribution
+
+n = 10000 # Total number of samples
+
+n1 = int(n*w1) # Number of samples from first distribution
+n2 = int(n*w2) # Number of samples from second distribution
+
+# Generate n1 samples from the first normal distribution and n2 samples from the second normal distribution
+X = concatenate((normal(mu1, sigma1, n1), normal(mu2, sigma2, n2))).reshape(-1, 1)
+
+# Determine parameters mu1, mu2, sigma1, sigma2, w1 and w2
+gm = GaussianMixture(n_components=2, random_state=0).fit(X)
+print(f'mu1={gm.means_[0]}, mu2={gm.means_[1]}')
+print(f'sigma1={np.sqrt(gm.covariances_[0])}, sigma2={np.sqrt(gm.covariances_[1])}')
+print(f'w1={gm.weights_[0]}, w2={gm.weights_[1]}')
+print(f'n1={int(n * gm.weights_[0])} n2={int(n * gm.weights_[1])}')
+
 st.line_chart(X)
+st.altair_chart(X)
 
 
 with st.expander("## Code", expanded=True):
